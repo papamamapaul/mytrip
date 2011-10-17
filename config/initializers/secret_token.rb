6af8fbda79e8d8846979a84c07ecacf539c1dafb
David Petereit <david.petereit@gmail.com>
2011-10-17 22:37:05 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mytrip::Application.config.secret_token = 'fd50565e14237a5b296b23225d900837ce3007798a3b03f2c88205a575d7ca00e6c3c716a84e82c951ca5da986be5947e9828a2ad0b1734ffd4c1cd385819ba3'
